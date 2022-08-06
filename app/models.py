922df2f55f0faff22c807609f8ff6ddefa12e877
Sam Thorold <sam.thorold@gmail.com>
2022-08-06 09:54:45 +0100

    Group names can change

--- app/models.py
+++ app/models.py
@@ -4,9 +4,6 @@ from pydantic import BaseModel, Field
 class Group(BaseModel):
     name: str = Field(..., min_length=1)
 
-    class Config:
-        frozen = True
-
 
 class CardCreate(BaseModel):
     front: str = Field(..., min_length=1)
