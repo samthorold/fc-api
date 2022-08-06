4bc8dffcdbc8239a5d746cdeea6ac76df3447ffc
Sam Thorold <sam.thorold@gmail.com>
2022-08-06 09:49:50 +0100

    #1 dynamic number of cards in new db fixture

--- conftest.py
+++ conftest.py
@@ -1,8 +1,33 @@
+from faker import Faker
 import pytest
 
+
+from app import db
 from app.app import get_app
+
+fake = Faker()
 
 
 @pytest.fixture
 def app():
     return get_app(".test.env")
+
+
+@pytest.fixture
+def new_db():
+    db.CARDSDB = []
+    db.GROUPSDB = []
+    return db
+
+
+def n_fake_cards(n: int = 10):
+    return [db.Card(front=fake.sentence(), back=fake.sentence()) for _ in range(n)]
+
+
+@pytest.fixture
+def non_empty_db(new_db, request):
+    m = request.node.get_closest_marker("num_cards")
+    if m and len(m.args) > 0:
+        for card in n_fake_cards(m.args[0]):
+            new_db.CARDSDB.append(card)
+    return new_db
--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 from httpx import AsyncClient
-from pydantic import BaseSettings
 import pytest
 
 
@@ -17,3 +16,11 @@ async def test_app_name(app):
         resp = await cl.get("/settings/")
     assert resp.status_code == 200
     assert resp.json()["app_name"] == "TestCards"
+
+
+@pytest.mark.num_cards(3)
+async def test_get_cards(app, non_empty_db):
+    async with AsyncClient(app=app, base_url="http://test") as cl:
+        resp = await cl.get("/cards/")
+    assert resp.status_code == 200
+    assert len(resp.json()) == 3, resp.json()
